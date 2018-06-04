3165582bafe0f87f47b46e8ef2b49d95fbcd4e4d
Jake-McDonald <jacrilmcd@gmail.com>
2018-06-04 13:27:36 -0300

    Added template fill for email task summary

--- setTemplate.js
+++ setTemplate.js
@@ -1,15 +1,26 @@
-var taskSummary = document.getElementById('ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id47:j_id51');
+var callWrapSummary = document.getElementById('ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id47:j_id51');
 var commentText = document.getElementById('ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id47:j_id52');
+var emailTaskSummary = document.getElementById('00N0P000006DxMi');
 
-if(taskSummary != null && commentText != null)
+//Set call wrap 
+if(callWrapSummary != null && commentText != null)
 {
-	taskSummary.style.height = "140px";
-	if(taskSummary == '')
+	callWrapSummary.style.height = "140px";
+	if(callWrapSummary == '')
 	{
-		taskSummary.value = "T2 Consult\nIB:\nOB:";
+		callWrapSummary.value = "T2 Consult\nIB:\nOB:";
 	}
 	if(commentText = '')
 	{
 		commentText.value = "NA";
+	}
+}
+
+if(emailTaskSummary != null)
+{
+	emailTaskSummary.style.height = "140px";
+	if(emailTaskSummary == '')
+	{
+		emailTaskSummary.value = "T2 Elevation\nIB:\nOB:";
 	}
 }
