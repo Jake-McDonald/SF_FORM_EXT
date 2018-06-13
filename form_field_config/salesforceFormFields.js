c9ae6420af72d222c2a27c2eeeee8ff2c04b4fd0
Jake-McDonald <jacrilmcd@gmail.com>
2018-06-13 16:08:03 -0300

    Added additional field IDs

--- form_field_config/salesforceFormFields.js
+++ form_field_config/salesforceFormFields.js
@@ -1,9 +1,12 @@
 var salesforceFormFields =
     {
         //Call Wrap window fields
+				"agentName": "ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id47:j_id49_id49",
+				"caseNumber": "ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id47:j_id50",
                 "callWrapSummary": "ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id47:j_id51",
                 "commentText": "ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id47:j_id52",
-                "saveButton": "ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id55:j_id56",
+                "callWrapSaveButton": "ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id55:j_id56",
+				"callResult": "ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id47:j_id53"
 
         //New task or Email task fields
                 "taskSummary": "00N0P000006DxMi"
