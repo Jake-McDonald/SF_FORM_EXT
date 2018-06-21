3ccb09e1363c94fc47ad6397e8ebd81d5aa51cd5
Jake-McDonald <jacrilmcd@gmail.com>
2018-06-21 11:57:52 -0300

    Added new IDs to salesForceFormFields

--- form_field_config/salesforceFormFields.js
+++ form_field_config/salesforceFormFields.js
@@ -1,10 +1,11 @@
-var salesforceFormFields;
-salesforceFormFields = {
     /*
     *This file needs to be updated any time that the tracker changes. You can obtain the field IDs by 
     *making a pre-filled URL on the Google Form. You need to have access to edit the form to make
     *a pre-filled URL.
     */
+
+var salesforceFormFields;
+salesforceFormFields = {
     
     //Call Wrap window fields
     "agentName": "ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id47:j_id49",
@@ -15,5 +16,7 @@ salesforceFormFields = {
     "callResult": "ACT_PostCallUpdateTaskLookups:j_id19:j_id20:j_id47:j_id53",
 
     //New task or Email task fields
-    "taskSummary": "00N0P000006DxMi"
+    "taskSummary": "00N0P000006DxMi",
+    "emailSaveButton": "save", //This is an HTML name tag, not an ID
+    "emailCaseNumber": "tsk3"
 };
